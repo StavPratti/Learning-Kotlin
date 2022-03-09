b37f99aa48ac6b19a37126e187581970bcabc6bf
Stavroula_Pratti <56409214+StavPratti@users.noreply.github.com>
2022-03-09 18:44:00 +0200

    Update variables.kt

--- variables.kt
+++ variables.kt
@@ -26,4 +26,6 @@ fun main() {
     var astr = '3'
     var bstr = '9'
     println(astr+name)
+
+    val name: String? = null // i put questionmark at the end of the type if i want to assign the null price to a var/val
 }
